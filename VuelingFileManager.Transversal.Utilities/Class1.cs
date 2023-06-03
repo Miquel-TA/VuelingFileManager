d982d1734b2793a095215a9ee98e736676f7c204
Miquel-TA <trazomobile4@gmail.com>
2023-06-03 16:59:45 +0200

    Agregar archivos de proyecto.

---
+++ VuelingFileManager.Transversal.Utilities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VuelingFileManager.Transversal.Utilities
+{
+    public class Class1
+    {
+    }
+}
