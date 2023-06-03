d8183c2cff826a679ea542d74c5465fb3a055789
Miquel-TA <trazomobile4@gmail.com>
2023-06-03 21:15:25 +0200

    Added student creation.

---
+++ VuelingFileManager.Business.StudentManager/StudentManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using VuelingFileManager.Infrastructure;
+using VuelingFileManager.Transversal.Utilities;
+using VuelingFileManager.Transversal.Utilities.Models;
+
+namespace VuelingFileManager.Business.Logic
+{
+    public class StudentManager
+    {
+        private readonly List<Student> students = new List<Student>();
+        private int studentCount = 0;
+
+        public int AddNewStudent(DateTime birthday, string name, string surname)
+        {
+            Student newStudent = new Student(students.Count, birthday, name, surname);
+            students.Add(newStudent);
+            studentCount++;
+            return studentCount;
+        }
+    }
+}
