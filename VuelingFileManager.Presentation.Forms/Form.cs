9ff6cb81c8a471e3a74a37774c4a43fcc08a7a09
Miquel-TA <trazomobile4@gmail.com>
2023-06-04 16:59:49 +0200

    Added logging system, exception managing and better input filtering.

--- VuelingFileManager.Presentation.Forms/Form.cs
+++ VuelingFileManager.Presentation.Forms/Form.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Drawing;
 using VuelingFileManager.Business.Logic;
 using VuelingFileManager.Transversal.Utilities;
+using VuelingFileManager.Transversal.Utilities.Models;
 
 namespace VuelingFileManager.Presentation.Form
 {
     public partial class Form : System.Windows.Forms.Form
     {
-
+        private readonly int minimumStudentAge = 18;
         private readonly StudentManager studentManager = new StudentManager();
 
         public Form()
@@ -18,17 +19,24 @@ namespace VuelingFileManager.Presentation.Form
 
         private void Form_Load(object sender, EventArgs e)
         {
-            birthdayInput.MinDate = DateTime.Now.AddYears(-120);
-            birthdayInput.MaxDate = DateTime.Now.AddYears(-18);
-            birthdayPrompt.Text = "Birthday";
-            namePrompt.Text = "Name";
-            surnamePrompt.Text = "Surname";
-            formatPrompt.Text = "Export format";
-            feedback.Text = "";
-            formatInput.DataSource = new List<string> { "TXT", "JSON", "XML" };
-            addButton.Text = "Add";
-            exportButton.Text = "Export";
-            clearButton.Text = "Clear";
+            try
+            {
+                birthdayInput.MinDate = DateTime.Now.AddYears(-120);
+                birthdayInput.MaxDate = DateTime.Now.AddYears(-minimumStudentAge);
+                birthdayPrompt.Text = "Birthday";
+                namePrompt.Text = "Name";
+                surnamePrompt.Text = "Surname";
+                formatPrompt.Text = "Export format";
+                feedback.Text = "";
+                formatInput.DataSource = new List<string> { "TXT", "JSON", "XML" };
+                addButton.Text = "Add";
+                exportButton.Text = "Export";
+                clearButton.Text = "Clear";
+                Logger.Log("UI initialized.", Logger.Severity.Info);
+            } catch (Exception ex)
+            {
+                Logger.Log(ex.Message, Logger.Severity.Critical);
+            }
         }
 
         private void exportButton_Click(object sender, EventArgs e)
@@ -37,27 +45,20 @@ namespace VuelingFileManager.Presentation.Form
             {
                 feedback.Text = "";
                 int studentCount = studentManager.GetStudentCount();
-                string format = formatInput.SelectedItem.ToString();
+                var format = formatInput.SelectedItem;
 
-                bool validFormat = VerifyExportFormat(format);
-                if (validFormat && studentCount > 0)
+                bool validInputs = VerifyExportInputs(format, studentCount);
+
+                if (validInputs)
                 {
-                    string exportPath = studentManager.ExportStudents(format);
-                    if (exportPath != null)
-                    {
-                        feedback.Text = $"Exported to {format}.";
-                    }
-                    else
-                    {
-                        feedback.Text = $"Export failed.";
-                    }
+                    string exportPath = studentManager.ExportStudents(format.ToString());
+                    feedback.Text = exportPath;
+                    Logger.Log($"Exported {studentCount} students to {exportPath}.", Logger.Severity.Info);
                 }
             } catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                Logger.Log(ex.Message, Logger.Severity.Critical);
             }
-
-
         }
 
         private void addButton_Click(object sender, EventArgs e)
@@ -69,16 +70,17 @@ namespace VuelingFileManager.Presentation.Form
                 string name = nameInput.Text;
                 string surname = surnameInput.Text;
 
-                bool validInputs = VerifyNewStudentValues(birthday, name, surname);
+                bool validInputs = VerifyStudentInputs(birthday, name, surname);
                 if (validInputs)
                 {
                     int studentId = studentManager.AddNewStudent(birthday, name, surname);
                     feedback.Text = $"Student {studentId} added.";
+                    Logger.Log($"Student {studentId} added.", Logger.Severity.Info);
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                Logger.Log(ex.Message, Logger.Severity.Critical);
             }
         }
 
@@ -88,47 +90,71 @@ namespace VuelingFileManager.Presentation.Form
             {
                 int studentsRemoved = studentManager.EmptyStudents();
                 feedback.Text = $"{studentsRemoved} students cleared.";
+                Logger.Log($"{studentsRemoved} students cleared.", Logger.Severity.Info);
             }
             catch (Exception ex)
             {
-
+                Logger.Log(ex.Message, Logger.Severity.Critical);
             }
         }
 
-        private bool VerifyExportFormat(string format)
+        private bool VerifyExportInputs(object format, int studentCount)
         {
-            if (!Validators.VerifyFormat(format))
+            if (studentCount > 0)
             {
-                formatPrompt.ForeColor = Color.Red;
-                feedback.Text = $"Please check the red fields.";
-                return false;
+                if (format != null)
+                {
+                    if (Validators.VerifyFormat(format.ToString()))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        formatPrompt.ForeColor = Color.Red;
+                        feedback.Text = $"Please check the red fields.";
+                        Logger.Log($"Format {format} is not valid for export.", Logger.Severity.Info);
+                        return false;
+                    }
+                }
+                else
+                {
+                    formatPrompt.ForeColor = Color.Red;
+                    feedback.Text = $"Please check the red fields.";
+                    Logger.Log($"Format is null, thus not valid for export.", Logger.Severity.Info);
+                    return false;
+                }
             }
             else
             {
-                return true;
+                feedback.Text = $"There are no students to export.";
+                Logger.Log($"StudentCount {studentCount} is not valid for export.", Logger.Severity.Info);
+                return false;
             }
         }
-        private bool VerifyNewStudentValues(DateTime birthday, string name, string surname)
+        private bool VerifyStudentInputs(DateTime birthday, string name, string surname)
         {
             bool validInputs = true;
             birthdayPrompt.ForeColor = Color.Black;
             namePrompt.ForeColor = Color.Black;
             surnamePrompt.ForeColor = Color.Black;
 
-            if (!Validators.VerifyDateTime(birthday))
+            if (!Validators.VerifyDateTime(birthday, minimumStudentAge))
             {
                 validInputs = false;
                 birthdayPrompt.ForeColor = Color.Red;
+                Logger.Log($"Birthday {birthday} not valid.", Logger.Severity.Info);
             }
             if (!Validators.VerifyName(name))
             {
                 validInputs = false;
                 namePrompt.ForeColor = Color.Red;
+                Logger.Log($"Name {name} not valid.", Logger.Severity.Info);
             }
             if (!Validators.VerifyName(surname))
             {
                 validInputs = false;
                 surnamePrompt.ForeColor = Color.Red;
+                Logger.Log($"Surname {surname} not valid.", Logger.Severity.Info);
             }
 
             if (!validInputs)
@@ -137,7 +163,6 @@ namespace VuelingFileManager.Presentation.Form
             }
 
             return validInputs;
-
         }
     }
 }
